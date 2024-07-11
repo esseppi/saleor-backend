2996fb4a56344fb6e8e1e1d2e52f3e70746cf642
Salvatore Pratico <salvatore@mindnow.io>
2024-07-12 01:18:59 +0200

    lol

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
-cd /var/www/esseppi-backend/
-if [ -f ".envs/.production/.django" ]; then
-  git checkout .envs/.production/.django
-fi
-git fetch -a
-git checkout origin/main
+cd /var/www/saleor-backend/
+# if [ -f ".envs/.production/.django" ]; then
+#   git checkout .envs/.production/.django
+# fi
+# git fetch -a
+# git checkout origin/main
 
 # gcloud secrets versions access latest --secret=esseppi-api-dev-env --format='get(payload.data)' | tr '_-' '/+' | base64 -d > /var/www/esseppi-backend/.envs/.production/.django
 
