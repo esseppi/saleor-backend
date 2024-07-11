8661d8f2daabe5278e928023eadb72337d41ce69
Salvatore Pratico <salvatore@mindnow.io>
2024-07-12 00:34:10 +0200

    test

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,15 +5,15 @@ if [ -f ".envs/.production/.django" ]; then
   git checkout .envs/.production/.django
 fi
 git fetch -a
-git checkout origin/development
+git checkout origin/main
 
 gcloud secrets versions access latest --secret=esseppi-api-dev-env --format='get(payload.data)' | tr '_-' '/+' | base64 -d > /var/www/esseppi-backend/.envs/.production/.django
 
 export DJANGO_SETTINGS_MODULE=saleor.settings
 
-docker-compose -f development.yml build
-docker-compose -f development.yml run --rm django python3 manage.py migrate
-docker-compose -f development.yml down
-docker-compose -f development.yml up -d
+docker-compose -f main.yml build
+docker-compose -f main.yml run --rm django python3 manage.py migrate
+docker-compose -f main.yml down
+docker-compose -f main.yml up -d
 
 docker system prune -af
